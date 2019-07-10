fe1ff7c5d1527a2d8332c53a37dd9c71af116b15
sudeepb02 <sudeepbiswas02@gmail.com>
2019-07-10 10:07:22 +0000

    Initial upload

--- src/mapping.ts
+++ src/mapping.ts
@@ -10,8 +10,12 @@ import {
   AuctionSuccessful,
   AuctionCancelled,
   Pause,
-  Unpause
+  Unpause,
+  SiringSummary,
+  DailyStatSiring
 } from "../generated/schema"
+
+import {BigInt} from '@graphprotocol/graph-ts'
 
 export function handleAuctionCreated(event: AuctionCreatedEvent): void {
   let entity = new AuctionCreated(
@@ -22,6 +26,41 @@ export function handleAuctionCreated(event: AuctionCreatedEvent): void {
   entity.endingPrice = event.params.endingPrice
   entity.duration = event.params.duration
   entity.save()
+
+  //Get Siring summary entity, if does not exist, create new
+  let siringSummary = SiringSummary.load("1");
+  if (siringSummary == null) {
+    siringSummary = new SiringSummary("1")
+    siringSummary.auctionsCreated = 0
+    siringSummary.auctionsCompleted = 0
+    siringSummary.auctionsCancelled = 0
+
+    siringSummary.valueCreated = BigInt.fromI32(0) 
+    siringSummary.valueCompleted = BigInt.fromI32(0)
+    siringSummary.valueCancelled = BigInt.fromI32(0)
+  }
+
+  //Increment the count and value of siring auctions
+  siringSummary.auctionsCreated = siringSummary.auctionsCreated + 1
+  siringSummary.valueCreated = siringSummary.valueCreated + event.params.startingPrice
+  
+  siringSummary.save()
+  
+  let day = event.block.timestamp / BigInt.fromI32(86400)
+  let dayID = day.toString()
+  let dailyStatSiring = DailyStatSiring.load(dayID)
+  if (dailyStatSiring == null) {
+    dailyStatSiring = new DailyStatSiring(dayID)
+    dailyStatSiring.auctionsCreatedToday = 0
+    dailyStatSiring.auctionsCompletedToday = 0
+    dailyStatSiring.auctionsCancelledToday = 0
+    dailyStatSiring.valueCreatedToday = BigInt.fromI32(0)
+    dailyStatSiring.valueCompletedToday = BigInt.fromI32(0)
+  }
+  dailyStatSiring.auctionsCreatedToday = dailyStatSiring.auctionsCreatedToday + 1
+  dailyStatSiring.valueCreatedToday = dailyStatSiring.valueCreatedToday + event.params.startingPrice
+  dailyStatSiring.save()
+  
 }
 
 export function handleAuctionSuccessful(event: AuctionSuccessfulEvent): void {
@@ -32,6 +71,41 @@ export function handleAuctionSuccessful(event: AuctionSuccessfulEvent): void {
   entity.totalPrice = event.params.totalPrice
   entity.winner = event.params.winner
   entity.save()
+
+  //Get Sales summary entity if exists, else create new
+  let siringSummary = SiringSummary.load("1");
+  if (siringSummary == null) {
+    siringSummary = new SiringSummary("1")
+    siringSummary.auctionsCreated = 0
+    siringSummary.auctionsCompleted = 0
+    siringSummary.auctionsCancelled = 0
+
+    siringSummary.valueCreated = BigInt.fromI32(0)
+    siringSummary.valueCompleted = BigInt.fromI32(0)
+    siringSummary.valueCancelled = BigInt.fromI32(0)
+  }
+
+  //Increment the count and value of siring auctions
+  siringSummary.auctionsCompleted = siringSummary.auctionsCompleted + 1
+  siringSummary.valueCompleted = siringSummary.valueCompleted + event.params.totalPrice
+  
+  siringSummary.save()
+  
+  let day = event.block.timestamp / BigInt.fromI32(86400)
+  let dayID = day.toString()
+  let dailyStatSiring = DailyStatSiring.load(dayID)
+  if (dailyStatSiring == null) {
+    dailyStatSiring = new DailyStatSiring(dayID)
+    dailyStatSiring.auctionsCreatedToday = 0
+    dailyStatSiring.auctionsCompletedToday = 0
+    dailyStatSiring.auctionsCancelledToday = 0
+    dailyStatSiring.valueCreatedToday = BigInt.fromI32(0)
+    dailyStatSiring.valueCompletedToday = BigInt.fromI32(0)
+  }
+  dailyStatSiring.auctionsCompletedToday = dailyStatSiring.auctionsCompletedToday + 1
+  dailyStatSiring.valueCompletedToday = dailyStatSiring.valueCompletedToday + event.params.totalPrice
+  dailyStatSiring.save()
+
 }
 
 export function handleAuctionCancelled(event: AuctionCancelledEvent): void {
@@ -40,6 +114,42 @@ export function handleAuctionCancelled(event: AuctionCancelledEvent): void {
   )
   entity.tokenId = event.params.tokenId
   entity.save()
+
+  //Get Sales summary entity if exists, else create new
+  let siringSummary = SiringSummary.load("1");
+  if (siringSummary == null) {
+    siringSummary = new SiringSummary("1")
+    siringSummary.auctionsCreated = 0
+    siringSummary.auctionsCompleted = 0
+    siringSummary.auctionsCancelled = 0
+
+    siringSummary.valueCreated = BigInt.fromI32(0)
+    siringSummary.valueCompleted = BigInt.fromI32(0)
+    siringSummary.valueCancelled = BigInt.fromI32(0)
+  }
+  
+  //Increment the count and value of siring auctions
+  siringSummary.auctionsCancelled = siringSummary.auctionsCancelled + 1
+  
+  //Currently, value Cancelled also includes uactions still open. To be fixed
+  siringSummary.valueCancelled = siringSummary.valueCreated - siringSummary.valueCompleted
+  
+  siringSummary.save()
+  
+  let day = event.block.timestamp / BigInt.fromI32(86400)
+  let dayID = day.toString()
+  let dailyStatSiring = DailyStatSiring.load(dayID)
+  if (dailyStatSiring == null) {
+    dailyStatSiring = new DailyStatSiring(dayID)
+    dailyStatSiring.auctionsCreatedToday = 0
+    dailyStatSiring.auctionsCompletedToday = 0
+    dailyStatSiring.auctionsCancelledToday = 0
+    dailyStatSiring.valueCreatedToday = BigInt.fromI32(0)
+    dailyStatSiring.valueCompletedToday = BigInt.fromI32(0)
+  }
+  dailyStatSiring.auctionsCancelledToday = dailyStatSiring.auctionsCancelledToday + 1
+  dailyStatSiring.save()
+
 }
 
 export function handlePause(event: PauseEvent): void {
